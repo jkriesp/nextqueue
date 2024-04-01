f5da42455851925531a23496d2068c42e22c481f
Jens Kristian Espevik <jenskristian@jenske.net>
2024-04-01 13:11:21 +0200

    update types

--- src/interfaces/SpotifyTypes.ts
+++ src/interfaces/SpotifyTypes.ts
@@ -1,18 +1,19 @@
-interface Image {
-    url: string;
-}
+// interface Image {
+//     url: string;
+// }
 
-interface Artist {
-    name: string;
-}
+// interface Artist {
+//     name: string;
+// }
 
-interface Track {
-    name: string;
-    artists: Artist[];
-    album: {
-        images: Image[];
-    };
-}
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// interface Track {
+//     name: string;
+//     artists: Artist[];
+//     album: {
+//         images: Image[];
+//     };
+// }
 
 export interface SpotifyData {
     currently_playing: {
